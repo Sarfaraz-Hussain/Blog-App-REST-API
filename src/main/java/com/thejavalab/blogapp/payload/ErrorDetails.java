99037f50f1beb4c0d248d397de81451bf5771f24
Sarfaraz-Hussain <call2me786@gmail.com>
2023-08-14 00:03:40 +0530

    exception handling code is added

---
+++ src/main/java/com/thejavalab/blogapp/payload/ErrorDetails.java
@@ -0,0 +1,15 @@
+package com.thejavalab.blogapp.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+}
